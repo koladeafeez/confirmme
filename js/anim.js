2e2f6eadb992364e36129ccbb578fb78c835546e
kolade Afeez <koladeafeez60@gmail.com>
2021-08-11 09:31:31 +0100

    add mapbox

---
+++ js/anim.js
@@ -0,0 +1,9 @@
+var animation = bodymovin.loadAnimation({
+  container: document.getElementById("animContainer"),
+  renderer: "svg",
+  loop: true,
+  autoplay: true,
+  path: "../assets/DrawKit - Animation Pack - Grape/PRODUCT/Animation 08/drawkit-grape-animation-8-LOOP.json", // lottie file path
+});
+
+// https://assets9.lottiefiles.com/packages/lf20_5n8yfkac.json
